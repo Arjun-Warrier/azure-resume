f26098e891c50662509089e8306bc103a1d130c7
Arjun Warrier <arjunwarrier166@gamil.com>
2025-03-25 22:52:23 +0530

    adding function changes

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     getVisitCount();
 })
 
-const functionApi = "http://localhost:7071/api/GetResumeCounter";
+const functionApi = "https://func-azureresume-arjun.azurewebsites.net/api/GetResumeCounter?code=9hQzR9lkEFJjHvvTYm7ToMUlGT-0IREeNcR7_kx2mxjZAzFujSO1cA==";
 
 const getVisitCount = () => {
     let count = 30;
